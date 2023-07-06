2a3c3ff27eacedabcf2b22c3074b4cc46d527db1
TeraxDC <terax.dc@gmail.com>
2023-07-06 13:44:15 -0500

    db and conexion

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -5,8 +5,9 @@
      public function conectar(){
     
      try{
-     $conexion=new PDO('mysql:host=localhost;dbname=xyltrylx_blog_huaytara', 'xyltrylx_blog_huaytara', 'bl0gHu4yt4r42023');
-     $conexion->exec("set names utf8;SET time_zone = 'America/Lima'");
+     $conexion=new PDO('mysql:host=localhost;dbname=blog', 'root', '');
+     //$conexion->exec("set names utf8;SET time_zone = 'America/Lima'");
+     $conexion->exec("set names utf8;");
      } catch (PDOException $e){
          echo 'Error de conexion a la base de datos'.$e->getMessage().":::".$e->getTraceAsString();
      }
